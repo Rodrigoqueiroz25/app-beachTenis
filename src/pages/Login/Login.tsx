160b7d77a90698e95b1d50c0cf550de4aab948ae
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-04-24 23:17:28 -0300

    feat(login): curved Bar component changed to Header Login and screens that used it were changed
    The curvedBar component had its name changed to HeaderLogin because, in addition to displaying the
    curved bar, it now displays another screen component that receives children, which is the title of
    the screen that is using the component.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,22 +4,24 @@ import img from '../../assets/Subtract.svg';
 import imgBeachTenis from '../../assets/player-beachTenis.svg';
 import imgMail from '../../assets/Mail.svg';
 import imgEye from '../../assets/eye.svg';
-import { CurvedBar } from '../../components/CurvedBar/CurvedBar';
 import { Button } from '../../components/Button/Button';
 import { FooterLogin } from '../../components/FooterLogin/FooterLogin';
 import { TextField } from '../../components/TextField/TextField';
+import { HeaderLogin } from '../../components/HeaderLogin/HeaderLogin';
 
 
 export function Login() {
     return (
         <div className={styles.login}>
-            <CurvedBar />
-            <header className={styles.header}>
-                <div className={styles.msgWelcome}>
-                    <p>Welcome</p>
-                    <p className={styles.back}>Back <img src={imgBeachTenis} alt="" /></p> 
+            <HeaderLogin>
+                <div className={styles.containerTitle}>        
+                    <div className={styles.msgWelcome}>
+                        <p>Welcome</p>
+                        <p className={styles.back}>Back <img src={imgBeachTenis} alt="" /></p> 
+                    </div>
                 </div>
-            </header>
+            </HeaderLogin>
+            
             <main>
                 <form className={styles.form} action="post">
                     <TextField placeholder='E-mail' type='email' src={imgMail}/>
--- src/pages/RegisterUser/RegisterUser.tsx
+++ src/pages/RegisterUser/RegisterUser.tsx
@@ -1,5 +1,4 @@
 
-import { CurvedBar } from '../../components/CurvedBar/CurvedBar';
 import styles from './RegisterUser.module.css';
 import imgBeachTenis from '../../assets/player-beachTenis.svg';
 import imgMail from '../../assets/Mail.svg';
@@ -7,17 +6,19 @@ import imgEye from '../../assets/eye.svg';
 import { TextField } from '../../components/TextField/TextField';
 import { Button } from '../../components/Button/Button';
 import { FooterLogin } from '../../components/FooterLogin/FooterLogin';
+import { HeaderLogin } from '../../components/HeaderLogin/HeaderLogin';
 
 export function RegisterUser(){
     return (
         <div className={styles.RegisterUser}>
-            <CurvedBar/>
-            <header className={styles.header}>
-                <div className={styles.msgWelcome}>
-                    <p>Hey <br /> Welcome</p>
-                    <img src={imgBeachTenis} alt="" />
+            <HeaderLogin>
+                <div className={styles.containerTitle}>        
+                    <div className={styles.msgWelcome}>
+                        <p>Hey <br /> Welcome</p>
+                        <img src={imgBeachTenis} alt="" />     
+                    </div>
                 </div>
-            </header>
+            </HeaderLogin>
             <main>
                 <form className={styles.form} action="post">
                     <div className={styles.inputPhoneNumber}>
