3cf6f6ea3001daa0f1c882d0623f98b1912a70e2
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-04-25 10:56:53 -0300

    feat(login): created CreatePassword screen but without functionality
    created CreatePassword screen but without functionality, just design

---
+++ src/pages/CreatePasswd/CreatePasswd.tsx
@@ -0,0 +1,34 @@
+
+
+import { HeaderLogin } from '../../components/HeaderLogin/HeaderLogin';
+import styles from './CreatePasswd.module.css';
+import imgCreatePasswd from '../../assets/create_passwd.svg';
+import { TextField } from '../../components/TextField/TextField';
+import { Button } from '../../components/Button/Button';
+import imgRectangle from '../../assets/Rectangle.svg';
+
+export function CreatePasswd(){
+    return (
+        <div className={styles.createPasswd}>
+            <HeaderLogin>
+                <div className={styles.containerTitle}>
+                    <p className={styles.title}>Create New Password</p>
+                </div>
+            </HeaderLogin>
+            
+            <main className={styles.containerMain}>
+                <img className={styles.imgCreatePasswd} src={imgCreatePasswd} alt="" />
+                <p className={styles.description}>Your New Password must be different from <br /> previous used Passwords</p>
+                <form className={styles.form} action="post">
+                    <TextField placeholder='Enter New Password' type='text'/>
+                    <TextField placeholder='Reenter New Password' type='text'/>
+                    <Button text='Login'/>
+                </form>                
+            </main>
+            
+            <footer className={styles.imgRetangle}>
+                <img src={imgRectangle} alt="" />
+            </footer>
+        </div>
+    );
+}
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -2,13 +2,15 @@ import { useRoutes } from "react-router-dom";
 import { Login } from "../pages/Login/Login";
 import { RegisterUser } from "../pages/RegisterUser/RegisterUser";
 import { ForgotPasswd } from "../pages/ForgotPasswd/ForgotPasswd";
+import { CreatePasswd } from "../pages/CreatePasswd/CreatePasswd";
 
 
 export function MainRoutes(){
     return useRoutes([
         { path: '/login', element: <Login/>},
         { path: '/registerUser', element: <RegisterUser/> },
-        { path: '/forgot-password', element: <ForgotPasswd/>}
+        { path: '/forgot-password', element: <ForgotPasswd/>},
+        { path: '/create-password', element: <CreatePasswd/> }
     ]);
 
 }
