882a96db796e495e719cba343ed6d9b2aa0f0728
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-04-27 17:18:47 -0300

    feat(login): component TextField modified to receive new parameters
    It receives new parameters like name, value, onChange and required.

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -4,13 +4,23 @@ import styles from './TextField.module.css';
 type Props = {
     placeholder: string;
     type: string;
+    name?: string;
+    value?: string;
+    func?: any;
     src?: string;
 }
 
-export function TextField({placeholder, type, src}: Props) {
+export function TextField({placeholder, type, name, value, func, src}: Props) {
     return (
         <div className={styles.textField}>
-            <input placeholder={placeholder} type={type} name="" />
+            <input 
+                placeholder={placeholder} 
+                type={type} 
+                name={name} 
+                value={value}
+                onChange={func}
+                required
+            />
             <img src={src} alt="" />
         </div>
     );
