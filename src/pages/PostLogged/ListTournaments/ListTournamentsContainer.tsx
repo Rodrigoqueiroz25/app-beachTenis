0bc0baa51ae3d781e185b47544abbcab2d601a39
Rodrigoqueiroz25 <digoqueiroz19@gmail.com>
2023-08-08 19:10:50 +0000

    feat: getting tournaments by filtering between ended and open on the listtournament screen

--- src/pages/PostLogged/ListTournaments/ListTournamentsContainer.tsx
+++ src/pages/PostLogged/ListTournaments/ListTournamentsContainer.tsx
@@ -1,39 +1,66 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes } from 'enums/routes.enum';
 import { useNavigate } from 'react-router-dom';
 import { PostLogged } from 'components/PostLogged';
 import { isAdmin } from 'helper/isAdmin';
 import { List } from './Presentation/List';
 import useTournament from 'hooks/useTournament';
-
+import styles from './styles.module.css'
 
 
 export function ListTournamentsContainer() {
 
-    const { getAllTournaments } = useTournament();
+    const [inProgress, setInProgress] = useState(true);
+
+    const { getAllTournamentsFilteredByDate } = useTournament();
     const navigate = useNavigate();
 
     useEffect(() => {
-        getAllTournaments.getAll();
-    }, [getAllTournaments.error]);
+        getAllTournamentsFilteredByDate.getAllFilteredByDate();
+    }, [getAllTournamentsFilteredByDate.error]);
+
+    function handleClickInProgress() {
+        setInProgress(true);
+    }
+
+    function handleClickFinished() {
+        setInProgress(false);
+    }
+
 
 
     return (
 
         <PostLogged.LayoutPage.Layout
             header={
-                <PostLogged.LayoutPage.Header>
-                    <PostLogged.ButtonBack onClick={() => navigate(Routes.home)} />
-                    <p>Torneios</p>
-                    {isAdmin() &&
-                        <PostLogged.ButtonPlus onClick={() => navigate(Routes.createTournament)} />
-                    }
-                </PostLogged.LayoutPage.Header>
+                <>
+                    <PostLogged.LayoutPage.Header>
+                        <PostLogged.ButtonBack onClick={() => navigate(Routes.home)} />
+                        <p>Torneios</p>
+                        {isAdmin() &&
+                            <PostLogged.ButtonPlus onClick={() => navigate(Routes.createTournament)} />
+                        }
+
+                    </PostLogged.LayoutPage.Header>
+                    <div className={styles.buttons}>
+                        <button
+                            value="andamento"
+                            onClick={handleClickInProgress}
+                            className={inProgress ? `${styles.focus}` : ""}
+                        >Em andamento</button>
+
+                        <button
+                            value="finalizado"
+                            onClick={handleClickFinished}
+                            className={!inProgress ? `${styles.focus}` : ""}
+                        >Finalizado</button>
+                    </div>
+                </>
             }
             main={
-                <List listTournaments={getAllTournaments.tournaments} />
+                <List listTournaments={inProgress ? getAllTournamentsFilteredByDate.tournaments?.opened : getAllTournamentsFilteredByDate.tournaments?.finished} />
             }
         />
     );
--- src/pages/PostLogged/ListTournaments/Presentation/List.tsx
+++ src/pages/PostLogged/ListTournaments/Presentation/List.tsx
@@ -6,50 +6,45 @@ import { Button } from 'components/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import { PostLogged } from 'components/PostLogged';
 import { isAdmin } from 'helper/isAdmin';
-import { ITournamentDataGetAllResponse } from 'interfaces/ITournament';
+import { ITournamentDataGetResponse } from 'interfaces/ITournament';
 
 
 
 interface ListProps {
-    listTournaments?: ITournamentDataGetAllResponse[];
+    listTournaments?: ITournamentDataGetResponse[];
 }
 
-export function List({listTournaments}: ListProps) {
+export function List({ listTournaments }: ListProps) {
 
     const navigate = useNavigate();
 
-    function access(tournament: ITournamentDataGetAllResponse): void {
+    function access(tournament: ITournamentDataGetResponse): void {
         navigate(`${Routes.tournamentLessParam}/${tournament.id}`)
     }
 
-    function configure(tournament: ITournamentDataGetAllResponse): void{
-        navigate(Routes.editTournament, { state: { tournament: tournament }});
+    function configure(tournament: ITournamentDataGetResponse): void {
+        navigate(Routes.editTournament, { state: { tournament: tournament } });
     }
 
     return (
-        <div className={styles.list}>
-            {listTournaments?.map((d: ITournamentDataGetAllResponse, key: number) => (
-                <PostLogged.Item.Wrapper key={key}>
-                    <div className={styles.itemList}>
-                        <img src={logoTour} alt="logo do torneio" />
-                        <PostLogged.Item.Period dtInit={d.dtStartTournament} dtFinal={d.dtFinalTournament} />
-                        <PostLogged.Item.Photos />
-                        <PostLogged.Item.Text text={d.organization} />
-                        <PostLogged.Item.Text small text={d.description} />
-                        {isAdmin()
-                            ?
-                            <>
-                                <Button small onClick={() => access(d)} >Acessar</Button>
+        <>
+            <div className={styles.list}>
+                {listTournaments?.map((d: ITournamentDataGetResponse, key: number) => (
+                    <PostLogged.Item.Wrapper key={key}>
+                        <div className={styles.itemList}>
+                            <img src={logoTour} alt="logo do torneio" />
+                            <PostLogged.Item.Period dtInit={d.dtStartTournament} dtFinal={d.dtFinalTournament} />
+                            <PostLogged.Item.Photos />
+                            <PostLogged.Item.Text text={d.organization} />
+                            <PostLogged.Item.Text small text={d.description} />
+                            <Button small onClick={() => access(d)} >Acessar</Button>
+                            {isAdmin() &&
                                 <Button small onClick={() => configure(d)}>Configurar</Button>
-                            </>
-                            :
-                            <>
-                                <Button small onClick={() => access(d)} >Acessar</Button>
-                            </>
-                        }
-                    </div>
-                </PostLogged.Item.Wrapper>
-            ))}
-        </div>
+                            }
+                        </div>
+                    </PostLogged.Item.Wrapper>
+                ))}
+            </div>
+        </>
     );
 }
